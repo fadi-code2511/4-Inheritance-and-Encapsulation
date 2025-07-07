9d13be728cce23c2caf2f6b3e2d2ff5194f4ccc3
Fadi nasr <nasr.fadi2511@gmail.com>
2025-07-07 22:25:15 +0300

    Implementing class student

--- Inheritance-and-Encapsulation.py
+++ Inheritance-and-Encapsulation.py
@@ -9,3 +9,14 @@ class User:
     def set_email(self,newEmail):
       self.__email=newEmail
       return self.__email
+    
+class Student(User):
+  def __init__(self, name, email):
+      super().__init__(name,email)
+      self.__enrolled_courses=[]
+
+  def enroll(self,course_name):
+    self.__enrolled_courses.append(course_name)
+  
+  def get_enrolled_courses(self):
+    return self.__enrolled_courses
